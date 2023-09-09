2c7946c87120a1c64de7c4ae86e9c61705b59f0e
Drew Olbrich <drew@lunarskydiving.com>
2023-09-09 09:28:16 -0700

    Initial Commit

---
+++ AttachmentControls/AttachmentControlsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AttachmentControlsApp.swift
+//  AttachmentControls
+//
+//  Created by Drew Olbrich on 9/9/23.
+//
+
+import SwiftUI
+
+@main
+struct AttachmentControlsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
